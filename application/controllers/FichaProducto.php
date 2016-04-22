3566b6f0f376b5d394258f9a087d71490d35d0a4
ferquinz <ferquinz_10@hotmail.com>
2016-04-22 20:45:57 +0200

    Clean project, one more button for transfer files and remove footer inside product modal

--- application/controllers/FichaProducto.php
+++ application/controllers/FichaProducto.php
@@ -24,7 +24,7 @@ class FichaProducto extends CI_Controller {
 
 		$this -> load -> helper('url');
 		$this -> load -> view("templates/header");
-		$this -> load -> view("fichaproducto", $data);
+		$this -> load -> view("fichaproducto", $data); 
 		$this -> load -> view('templates/fin');
 	}
 		
